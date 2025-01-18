452071d49b0ebba656c6e9633c24f494ed71e6af
baiqueee <hughdev101@gmail.com>
2025-01-18 12:03:38 +0700

    refactor: update DonationDetail component to handle async params; improve error handling in handle-error module

--- src/app/(product)/donations/[id]/page.tsx
+++ src/app/(product)/donations/[id]/page.tsx
@@ -3,7 +3,8 @@ import DonationDetailComponent from "@/components/donations/DonationDetail";
 
 export const dynamic = "force-dynamic";
 
-function DonationDetail({ params }: { params: { id: string } }) {
+async function DonationDetail(props: { params: Promise<{ id: string }> }) {
+  const params = await props.params;
   return (
     <main>
       <div className="py-6 sm:py-12 md:py-24 lg:h-[calc(100vh-3.5rem)]">
